673b637ecdc64f9a03ed418330ad2c650a255561
aryanchy4499 <158821779+aryanchy4499@users.noreply.github.com>
2024-07-22 17:15:16 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,7 +8,7 @@ API_HASH = os.environ.get("API_HASH","b3611c291bf82d917637d61e4a136535")
 # For /log cmd
 OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "7011929837").split(" ")]
 # No time limit for this users
-AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "7011929837").split(" ")]
+AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "6023712723").split(" ")]
 # Time gap after each request (in seconds)
 TIME_GAP = int(os.environ.get("TIME_GAP", "10"))
 # Bot channel ID for ForceSub, don't forget to add bot in Bot Channel
