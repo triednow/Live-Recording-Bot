6c5f549057c6d126b3d6ab6759a038e10a5e49e4
aryanchy4499 <158821779+aryanchy4499@users.noreply.github.com>
2024-07-22 17:14:43 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,13 +3,13 @@ import os
 # Bot token from @botfather
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 # From my.telegram.org/
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH")
+API_ID = int(os.environ.get("API_ID", "23080322"))
+API_HASH = os.environ.get("API_HASH","b3611c291bf82d917637d61e4a136535")
 # For /log cmd
-OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "1204927413").split(" ")]
+OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "7011929837").split(" ")]
 # No time limit for this users
-AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "1204927413").split(" ")]
+AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "7011929837").split(" ")]
 # Time gap after each request (in seconds)
-TIME_GAP = int(os.environ.get("TIME_GAP", "360"))
+TIME_GAP = int(os.environ.get("TIME_GAP", "10"))
 # Bot channel ID for ForceSub, don't forget to add bot in Bot Channel
 UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", False)
